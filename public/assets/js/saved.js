9168e10a2fe619aa575361d95233a73aecc30a70
Jeffery Saephan <jeffery.saephan@gmail.com>
2019-12-06 22:33:00 -0800

    fx for deleting notes

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -101,7 +101,7 @@ $(document).ready(function() {
         // Now append the notesToRender to the note-container inside the note modal
         $(".note-container").append(notesToRender);
       }
-      
+
       // This function handles deleting articles/headlines
       function handleArticleDelete() {
         var articleToDelete = $(this)
@@ -122,3 +122,76 @@ $(document).ready(function() {
           }
         });
       }
+      // This function handles opening the notes modal and displaying our notes
+      function handleArticleNotes(event) {
+        var currentArticle = $(this)
+          .parents(".card")
+          .data();
+        // Grab any notes with this headline/article id
+        $.get("/api/notes/" + currentArticle._id).then(function(data) {
+          // Constructing our initial HTML to add to the notes modal
+          var modalText = $("<div class='container-fluid text-center'>").append(
+            $("<h4>").text("Notes For Article: " + currentArticle._id),
+            $("<hr>"),
+            $("<ul class='list-group note-container'>"),
+            $("<textarea placeholder='New Note' rows='4' cols='60'>"),
+            $("<button class='btn btn-success save'>Save Note</button>")
+          );
+          // Adding the formatted HTML to the note modal
+          bootbox.dialog({
+            message: modalText,
+            closeButton: true
+          });
+          var noteData = {
+            _id: currentArticle._id,
+            notes: data || []
+          };
+          // Adding some information about the article and article notes to the save button for easy access
+          // When trying to add a new note
+          $(".btn.save").data("article", noteData);
+          // renderNotesList will populate the actual note HTML inside of the modal we just created/opened
+          renderNotesList(noteData);
+        });
+      }
+
+      // This function handles what happens when a user tries to save a new note for an article
+  function handleNoteSave() {
+    // Setting a variable to hold some formatted data about our note,
+    var noteData;
+    var newNote = $(".bootbox-body textarea")
+      .val()
+      .trim();
+    // If we actually have data typed into the note input field, format it
+    // and post it to the "/api/notes" route and send the formatted noteData as well
+    if (newNote) {
+      noteData = { _headlineId: $(this).data("article")._id, noteText: newNote };
+      $.post("/api/notes", noteData).then(function() {
+        // When complete, close the modal
+        bootbox.hideAll();
+      });
+    }
+  }
+
+  // This function handles the deletion of notes
+  function handleNoteDelete() {
+    // First we grab the id of the note we want to delete
+    // We stored this data on the delete button when we created it
+    var noteToDelete = $(this).data("_id");
+    // Perform an DELETE request to "/api/notes/" with the id of the note we're deleting as a parameter
+    $.ajax({
+      url: "/api/notes/" + noteToDelete,
+      method: "DELETE"
+    }).then(function() {
+      // When done, hide the modal
+      bootbox.hideAll();
+    });
+  }
+
+  function handleArticleClear() {
+    $.get("api/clear")
+      .then(function() {
+        articleContainer.empty();
+        initPage();
+      });
+  }
+});
