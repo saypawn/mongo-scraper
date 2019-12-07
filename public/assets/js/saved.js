47f9e44f9b92b6cbc71fd1535c982da59b8f17aa
Jeffery Saephan <jeffery.saephan@gmail.com>
2019-12-06 22:14:53 -0800

    inital save code

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -1,13 +1,17 @@
-/* global bootbox */
+//code for saving notes
 $(document).ready(function() {
     var articleContainer = $(".article-container");
-    $(document).on("click", ".btn.save", handleArticleSave);
-    $(document).on("click", ".scrape-new", handleArticleScrape);
+    // Adding event listeners for buttons for deleting articles,
+    // pulling up article notes, saving article notes, and deleting article notes
+    $(document).on("click", ".btn.delete", handleArticleDelete);
+    $(document).on("click", ".btn.notes", handleArticleNotes);
+    $(document).on("click", ".btn.save", handleNoteSave);
+    $(document).on("click", ".btn.note-delete", handleNoteDelete);
     $(".clear").on("click", handleArticleClear);
   
     function initPage() {
-      // Run an AJAX request for any unsaved headlines
-      $.get("/api/headlines?saved=false").then(function(data) {
+      // Empty the article container, run an AJAX request for any saved headlines
+      $.get("/api/headlines?saved=true").then(function(data) {
         articleContainer.empty();
         // If we have headlines, render them to the page
         if (data && data.length) {
