75389b3487032f0e3b3ab5700622f78e0f70f2fb
Jeffery Saephan <jeffery.saephan@gmail.com>
2019-12-06 18:27:42 -0800

    fx for users to save articles

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -71,4 +71,29 @@ $(document).ready(function() {
         articleContainer.append(emptyAlert);
       }
 
+        // This function is triggered when the user wants to save an article
+      function handleArticleSave() {
+        var articleToSave = $(this)
+          .parents(".card")
+          .data();
+    
+        // Remove card from page
+        $(this)
+          .parents(".card")
+          .remove();
+    
+        articleToSave.saved = true;
+        // Using a patch method to be semantic since this is an update to an existing record in our collection
+        $.ajax({
+          method: "PUT",
+          url: "/api/headlines/" + articleToSave._id,
+          data: articleToSave
+        }).then(function(data) {
+          // If the data was saved successfully
+          if (data.saved) {
+            // Run the initPage function again. This will reload the entire list of articles
+            initPage();
+          }
+        });
+      }
 
